f800c2b26481cba0c3eb946818e9b8ea80255998
Uncled1023 <admin@teknik.io>
2016-09-23 16:45:17 -0700

    Updated Nuget Packages. Added Searx functionality to search module.

--- Modules/Search/Search.cs
+++ Modules/Search/Search.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace Combot.Modules.Plugins
 {
@@ -25,6 +26,9 @@ namespace Combot.Modules.Plugins
                     GoogleSearch(command);
                     break;
                 case "Bing":
+                    break;
+                case "Searx":
+                    SearxSearch(command);
                     break;
             }
         }
@@ -77,6 +81,73 @@ namespace Combot.Modules.Plugins
                     string errorCode = string.Format("Unable to search for \u0002{0}\u000F.", command.Arguments["Query"]);
                     SendResponse(command.MessageType, command.Location, command.Nick.Nickname, errorCode);
                 }
+            }
+        }
+
+        private void SearxSearch(CommandMessage command)
+        {
+            JArray hosts = (JArray)GetOptionValue("Hosts");
+            JArray engines = (JArray)GetOptionValue("Engines");
+
+            WebException curException = new WebException();
+            bool hasError = false;
+            bool hasNoResults = false;
+
+            // Step through each host until we get a valid response
+            foreach (string host in hosts)
+            {
+                string urlTemplate = "{0}?q={1}&engines={2}&format=json";
+                Uri searchUrl = new Uri(string.Format(urlTemplate, host, HttpUtility.UrlEncode(command.Arguments["Query"]), string.Join(",", engines)));
+
+                WebClient web = new WebClient();
+                web.Encoding = Encoding.UTF8;
+                web.Credentials = CredentialCache.DefaultCredentials;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                try
+                {
+                    string page = web.DownloadString(searchUrl);
+
+                    JObject parsed = (JObject)JsonConvert.DeserializeObject(page);
+                    if (parsed["results"].Any())
+                    {
+                        var result = parsed["results"][0];
+                        string url = result.Value<string>("url");
+                        string title = HttpUtility.HtmlDecode(HttpUtility.UrlDecode(StripTagsCharArray(result.Value<string>("title"))));
+                        string content = HttpUtility.HtmlDecode(HttpUtility.UrlDecode(StripTagsCharArray(result.Value<string>("content"))));
+                        string resultMessage = string.Format("[{0}] \u0002{1}\u000F: {2}.", url, title, content);
+                        SendResponse(command.MessageType, command.Location, command.Nick.Nickname, resultMessage);
+                        return;
+                    }
+                    else
+                    {
+                        hasNoResults = true;
+                    }
+                }
+                catch (WebException ex)
+                {
+                    hasError = true;
+                    curException = ex;
+                }
+            }
+            if (hasError && !hasNoResults)
+            {
+                if (curException.Response != null)
+                {
+                    int code = (int)((HttpWebResponse)curException.Response).StatusCode;
+                    string errorCode = string.Format("Unable to search for \u0002{0}\u000F.  Returned status code \u0002{1}\u000F.", command.Arguments["Query"], code);
+                    SendResponse(command.MessageType, command.Location, command.Nick.Nickname, errorCode);
+                }
+                else
+                {
+                    string errorCode = string.Format("Unable to search for \u0002{0}\u000F.", command.Arguments["Query"]);
+                    SendResponse(command.MessageType, command.Location, command.Nick.Nickname, errorCode);
+                }
+            }
+            else
+            {
+                string noResults = string.Format("No results found for \u0002{0}\u000F.", command.Arguments["Query"]);
+                SendResponse(command.MessageType, command.Location, command.Nick.Nickname, noResults);
             }
         }
 
